c499bd59c6d4856216b1ea58a0ad28207bff1c01
fadamaka <tamas.hercz@cubicfox.com>
2022-12-13 22:58:56 +0100

    day 8

---
+++ day_8/trees.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+
+try {
+  const data = fs.readFileSync("data.txt", "utf8");
+  const trees = data.split("\n");
+  const ints = trees.map((a) => a.split("").map((c) => parseInt(c)));
+  console.log(
+    trees[0].length,
+    ints.length,
+    ints[0].length,
+    (ints.length + ints[0].length - 2) * 2
+  );
+  let result = (ints.length + ints[0].length - 2) * 2;
+  for (let i = 1; i < 98; i++) {
+    for (let j = 1; j < 98; j++) {
+      result += determineVisibility(i, j, ints) ? 1 : 0;
+    }
+  }
+  console.log(result);
+} catch (err) {
+  console.error(err);
+}
+
+function determineVisibility(x, y, array) {
+  let tree = array[x][y];
+  let up = true;
+  let down = true;
+  let left = true;
+  let right = true;
+  for (let i = x - 1; i >= 0; i--) {
+    if (tree <= array[i][y]) {
+      up = false;
+      break;
+    }
+  }
+  if (up) {
+    return up;
+  }
+  for (let i = x + 1; i < 99; i++) {
+    if (tree <= array[i][y]) {
+      down = false;
+      break;
+    }
+  }
+  if (down) {
+    return down;
+  }
+  for (let i = y - 1; i >= 0; i--) {
+    if (tree <= array[x][i]) {
+      right = false;
+      break;
+    }
+  }
+  if (right) {
+    return right;
+  }
+  for (let i = y + 1; i < 99; i++) {
+    if (tree <= array[x][i]) {
+      left = false;
+      break;
+    }
+  }
+  if (left) {
+    return left;
+  }
+  return false;
+}

014d7d67c171c535a3534f1c3b727203fef73b1c
fadamaka <tamas.hercz@cubicfox.com>
2022-12-20 21:55:12 +0100

    day 17 semi manual math solution

--- day_17/tetris-2.js
+++ day_17/tetris-2.js
@@ -1,5 +1,11 @@
 const fs = require("fs");
+
+let myLogFileStream = fs.createWriteStream("output.log");
+
+let myConsole = new console.Console(myLogFileStream, myLogFileStream);
+console.time("execution time");
 let map = [[1, 1, 1, 1, 1, 1, 1]];
+let mapLengthOffset = 0;
 const offsetObj = {
   "+": [
     [0, 0],
@@ -40,16 +46,26 @@ let data = "";
 let dataIndex = "";
 try {
   data = fs.readFileSync("data.txt", "utf8");
-  for (let i = 0; i < 2022; i++) {
+  for (let i = 0; i < 1000000000000 - 999999996840; i++) {
     shapesIndex = shapesIndex < shapes.length ? shapesIndex : 0;
     let rockObj = new Rock(shapes[shapesIndex++]);
     while (moveRock(rockObj)) {}
     stopRock(rockObj);
+    if (map.length + mapLengthOffset - 1 == 2528) {
+      console.log(map.length + mapLengthOffset - 1, i);
+    }
+    if ((map.length + mapLengthOffset - 1) % 2626 == 2527) {
+      console.log(map.length + mapLengthOffset - 1, i);
+    }
   }
-  console.log(map.length - 1);
+  drawMap();
+
+  console.log(map.length + mapLengthOffset - 1 + 581395347 * 2626);
 } catch (err) {
   console.error(err);
 }
+
+console.timeEnd("execution time");
 
 function Rock(sign) {
   this.sign = sign;
@@ -78,9 +94,6 @@ function moveRock(rock) {
     return false;
   }
 }
-function moveRockDown(rock) {
-  return checkCollision(rock, 0, -1);
-}
 function checkCollision(rock, x, y) {
   if (x != 0) {
     let xCoords = rock.offsets.map((o) => rock.x + o[0] + x);
@@ -100,18 +113,28 @@ function checkCollision(rock, x, y) {
     .filter((e) => e);
 }
 function stopRock(rock) {
-  let height = Math.max(...rock.offsets.map((o) => rock.y + o[1]));
+  let allX = rock.offsets.map((o) => rock.y + o[1]);
+  let height = Math.max(...allX);
+  let min = Math.min(...allX);
   while (map.length < height + 1) {
     map.push([0, 0, 0, 0, 0, 0, 0]);
   }
+
   solidifyRock(rock);
+  if (map[min].every((e) => e == 1)) {
+    for (let y = min - 1; y >= 0; y--) {
+      myConsole.log(map[y].reduce((a, b) => a + b, ""));
+    }
+    map = map.slice(min);
+    mapLengthOffset += min;
+  }
 }
 function solidifyRock(rock) {
   rock.offsets.forEach((o) => (map[rock.y + o[1]][rock.x + o[0]] = 1));
 }
 function drawMap() {
   for (let y = map.length - 1; y >= 0; y--) {
-    console.log(map[y].reduce((a, b) => a + b, ""));
+    myConsole.log(map[y].reduce((a, b) => a + b, ""));
   }
 }
 function getGasDirection() {

7cc56e3f6922a663cf7cc42432d642bf0a3df5c1
fadamaka <tamas.hercz@cubicfox.com>
2022-12-22 00:13:40 +0100

    day 20 pt1

---
+++ day_20/mix.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+
+let arr = fs
+  .readFileSync("data.txt", "utf8")
+  .split("\n")
+  .map((e) => ({ num: parseInt(e), moved: false }));
+
+//console.log(arr.reduce((a, b) => a + b.num + ",", ""));
+for (let i = 0; i < arr.length; i++) {
+  let num = arr[i];
+  if (num.moved) {
+    continue;
+  }
+  //console.log(num.num, "moves");
+  let newIndex = (num.num % arr.length) + i;
+  newIndex =
+    newIndex < 0
+      ? arr.length + newIndex
+      : (newIndex % arr.length) + (num.num < 0 ? 0 : 1);
+  if (num.num < 0 - arr.length || num.num >= arr.length) {
+    newIndex += Math.floor(num.num / arr.length);
+    newIndex += num.num / arr.length < 0 ? 1 : 0;
+  }
+
+  num.moved = true;
+  arr.splice(newIndex, 0, num);
+  if (newIndex < i) {
+    arr.splice(i + 1, 1);
+  } else {
+    arr.splice(i, 1);
+  }
+  if (newIndex > i) {
+    i--;
+  }
+  //console.log(arr.reduce((a, b) => a + b.num + ",", ""));
+}
+let numArr = arr.map((o) => o.num);
+let results = [1000, 2000, 3000];
+console.log(
+  results.reduce(
+    (a, b) => a + numArr[(b + numArr.indexOf(0)) % numArr.length],
+    0
+  )
+);
